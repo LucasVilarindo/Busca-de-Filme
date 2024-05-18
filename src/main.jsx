9bfd48f81938aabdd8ed333ff590ba9092329d51
LucasVilarindo <lucasvilarindo@outlook.com>
2024-05-18 19:49:10 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BuscaDeFilmes from './BuscaDeFilmes.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuscaDeFilmes />
+  </React.StrictMode>,
+)
